7a71b98b390c776e7706aab07c81ae70983cda74
marcusvolz <marcus.volz@outlook.com>
2017-05-29 21:52:24 +1000

    added k-nearest neighbour code and plot

--- master.R
+++ master.R
@@ -27,3 +27,17 @@ result <- deldir(points)
 voronoi <- result$dirsgs
 p3 <- p + geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), voronoi)
 ggsave("plots/003-voronoi.png", p3, width = 20, height = 20, units = "in")
+
+# Nearest neigbour
+find_nearest <- function(points, id, n) {
+  xi <- points$x[id]
+  yi <- points$y[id]
+  temp <- points %>% mutate(dist = sqrt((x - xi)^2 + (y - yi)^2)) %>% arrange(dist)
+  result <- temp[seq(2, n + 1), ] %>% mutate(xend = xi, yend = yi)
+}
+
+result <- 1:10000 %>%
+  map_df(~find_nearest(points, ., 8), .id = "id")
+
+p4 <- p + geom_segment(aes(x, y, xend = xend, yend = yend), result)
+ggsave("plots/004-08-nn.png", p4, width = 20, height = 20, units = "in")
