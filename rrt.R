d14f0165a5821b5a553e71906d5c22169b10dd80
marcusvolz <marcus.volz@outlook.com>
2017-12-03 21:22:08 +1100

    tweaking parameters

--- rrt.R
+++ rrt.R
@@ -14,7 +14,7 @@ set.seed(10000)
 # Set parameters
 n <- 1000000 # Number of iterations
 X <- 10000 # Square canvas dimension
-delta <- 2
+delta <- 1000
 
 # Create data frame
 points <- data.frame(x = numeric(n), y = numeric(n))
@@ -52,24 +52,29 @@ while(i <= n) {
   print(i)
 }
 
-edges2 <- edges[1:50000, ] %>% mutate(id = 1:nrow(.), frame = floor(id / (50000 / 100)))
+edges2 <- edges[1:50000, ] %>% mutate(id = 1:nrow(.), frame = floor(id / (50000 / 100))) %>%
+  filter(x > 0)
+
+#edges2 <- readRDS("edges.RDS") %>% mutate(id = 1:nrow(.)) %>% filter(x > 0)
 
 # Create plot
-p <- ggplot(mapping = aes(frame = frame, cumulative = FALSE)) +
-  geom_segment(aes(x, y, xend = xend, yend = yend),
-             edges2, lineend = "round", size = 2.5) +
+p <- ggplot() +
+  geom_segment(aes(x, y, xend = xend, yend = yend, size = -id, alpha = -id),
+             edges2, lineend = "round") +
   #xlim(0, 10000) +
   #ylim(0, 10000) +
   coord_equal() +
+  scale_size_continuous(range = c(0.1, 0.75)) +
+  scale_alpha_continuous(range = c(0.1, 1)) +
+  #scale_colour_gradient(high = "white", low = 'black') +
   theme_blankcanvas(margin_cm = 0)
   #theme_void() + theme(legend.position = "none")
 
 # Save plot
 #ggsave(paste("rrt_delta___", delta, ".png", sep = ""), p, width = 20, height = 20, units = "cm", dpi = 300)
-ggsave("rrt001.png", p, width = 20, height = 20, units = "cm", dpi = 300)
+ggsave("rrt003.png", p, width = 20, height = 20, units = "cm", dpi = 450)
 
-
-animation::ani.options(interval = 1/30)
-
-gganimate(p, "rrt001.gif", title_frame = FALSE, ani.width = 750, 
-           ani.height = 750)
+# animation::ani.options(interval = 1/30)
+# 
+# gganimate(p, "rrt001.gif", title_frame = FALSE, ani.width = 750, 
+#            ani.height = 750)
